793f70a2449a0cf84d8e58f5dc2797d6c7e136e5
Haritha113 <41508135+Haritha113@users.noreply.github.com>
2020-07-10 10:41:43 +0530

    Update Testscript.py

--- Testscript.py
+++ Testscript.py
@@ -3,3 +3,11 @@ print("YOur Python script pulled from github to jenkins")
 print("Hi Haritha")
 for i in range(0,5)
   print(i)
+def search(arr, x): 
+  
+    for i in range(len(arr)): 
+  
+        if arr[i] == x: 
+            return i 
+  
+    return -1
